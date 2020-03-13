b729d62dc18a7291c0fb2d4a2cf93add8211bc12
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-13 11:36:47 +0100

    React element

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello Udemy !');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const el =(
+    <div>
+    <h1> My To-Do List</h1>
+        <input  placeholder="search" />
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    </div>
+) ;
+    console.log(el);
+
+
+ReactDom.render(el, document.getElementById('root'));

014e0d8a1cf9b764dda1f961f34ed115edcf1a17
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-14 12:39:05 +0100

    Updates

---
+++ precache-manifest.fdb3a44131648a9a0aae404cbe27962b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb10fe99d8f33f7b79fc35ba02b9f27",
+    "url": "/Todo_Ude%E2%80%A6/index.html"
+  },
+  {
+    "revision": "aa65d4c7b948b7e0affb",
+    "url": "/Todo_Ude%E2%80%A6/static/css/main.db29476f.chunk.css"
+  },
+  {
+    "revision": "a14d339d916873208c1c",
+    "url": "/Todo_Ude%E2%80%A6/static/js/2.e7b44f85.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/Todo_Ude%E2%80%A6/static/js/2.e7b44f85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa65d4c7b948b7e0affb",
+    "url": "/Todo_Ude%E2%80%A6/static/js/main.867f680e.chunk.js"
+  },
+  {
+    "revision": "8094fcfe7b29a2218c01",
+    "url": "/Todo_Ude%E2%80%A6/static/js/runtime-main.84624457.js"
+  }
+]);

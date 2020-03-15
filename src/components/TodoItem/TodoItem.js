1638be4ad83d4ea3e817dd217e28249e3fb64fb0
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-15 13:48:14 +0100

    Component Class, Event Handling, state

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -8,34 +8,53 @@ import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Button from '@material-ui/core/Button';
 
+class TodoItem extends React.Component{
+    state ={
+        done: false
+    };
+    onLabelClick = () => {
+        this.setState({
+            done: true
+        })
+    };
 
-const TodoItem = ({label, important = false}) => {
-    return (
-        <div>
-        <Checkbox
-            icon={<FavoriteBorder />}
-            checkedIcon={<Favorite />}
-            value="checkedH" />
-        <span
-            className={ classnames ({
-                [styles.item]:true,
-                [styles.important]:important,
-                [styles.label]: true,
-                [styles.text]: true
+    render(){
 
-        })}>{label}</span>
+        const {label, important = false} = this.props;
+        const {done} = this.state;
+        let classNames = classnames ({
+            [styles.item]:true,
+             [styles.important]:important,
+            [styles.label]: true,
+            [styles.text]: true,
+            [styles.done]: done
+        });
+        return (
+            <div>
+                <Checkbox
+                    icon={<FavoriteBorder />}
+                    checkedIcon={<Favorite />}
+                    value="checked" />
+                <span
+                    className={classNames }
+                    onClick={this.onLabelClick}
+                >
+                {label}
+                </span>
 
-        <div>
-            <IconButton aria-label="delete" href="#">
-                <DeleteIcon fontSize="large" />
-            </IconButton>
-            <div className={styles.button}>
-            <Button color="secondary" href="#"   >!</Button>
+                <div>
+                    <IconButton aria-label="delete" href="#">
+                        <DeleteIcon fontSize="large" />
+                    </IconButton>
+                    <div className={styles.button}>
+                        <Button color="secondary" href="#"   >!</Button>
+                    </div>
+                </div>
             </div>
-            </div>
-        </div>
 
-    )
-};
+        )
+    }
+}
+
 
 export default TodoItem;

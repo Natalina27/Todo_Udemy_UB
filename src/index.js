d97d5b3b2707b597ea1fd50b80b16c4140502be5
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-24 15:30:11 +0100

    1. React element

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const el = (
-    <div>
-        <h1> My To-Do List</h1>
-        <input placeholder='search'/>
-        <ul>
-            <li>Learn react</li>
-            <li>Build awesome app</li>
-        </ul>
-    </div>
-);
+const ItemList = () => {
+    return (
+        <div>
+            <h1> My To-Do List</h1>
+            <ul>
+                <li>Learn react</li>
+                <li>Build awesome app</li>
+            </ul>
+        </div>
+    )
+};
 
-ReactDom.render(el, document.getElementById('root'));
+const SearchItem = () => {
+    return <input placeholder='search'/>;
+};
+
+const InputItem = () => {
+    return <input placeholder='add new element'/>;
+};
+
+const App = () => {
+    return(
+        <div>
+            <SearchItem/>
+            < ItemList/>
+            <InputItem/>
+        </div>
+    )
+};
+
+
+
+ReactDom.render(<App/>, document.getElementById('root'));

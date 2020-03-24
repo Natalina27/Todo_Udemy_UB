187bdbbda47d664ffb2b6b339ae56e9dd61a6967
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-24 20:29:15 +0100

    Arrays like props

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import Item from "../Item/Item";
+
+
 const ItemList = ({items}) => {
+    const elements = items.map((item) => {
+        return (
+            <li>
+                < Item {...item}/>
+            </li>
+        );
+    });
+
     return (
         <div>
             <h1> My To-Do List</h1>
             <ul>
-                <li>< Item
-                    label={items[0].label}
-                    important={items[0].important}/></li>
-                <li>< Item
-                    label={items[1].label}
-                    important={items[1].important}/></li>
-                <li>< Item
-                    label={items[2].label}
-                    important={items[2].important}/></li>
+                {elements}
             </ul>
         </div>
     )

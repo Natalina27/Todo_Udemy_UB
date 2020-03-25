8fcc910cbb104825575b3f1117ed7cef812f0544
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-25 20:34:28 +0100

    MUI styles

--- src/components/SearchPanel/SearchItem.jsx
+++ src/components/SearchPanel/SearchItem.jsx
@@ -4,10 +4,12 @@ import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
+import ItemStatusFilter from "../ItemStatusFilter/ItemStatusFilter";
 
 const SearchItem = () => {
 
     return (
+        <div className={styles.search}>
         <Paper component="form" className={styles.root} >
             <IconButton type="submit"  aria-label="search" href='#'>
                 <SearchIcon />
@@ -16,6 +18,8 @@ const SearchItem = () => {
                 placeholder=" Search "
                 inputProps={{ 'aria-label': 'search ' }}/>
         </Paper>
+        <ItemStatusFilter/>
+        </div>
 
     )
 };

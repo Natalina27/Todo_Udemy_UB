1dd16c28acb7382191b2cc846f047888bd63c4f0
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-25 11:50:15 +0100

    MUI styles

--- src/components/InputItem/InputItem.jsx
+++ src/components/InputItem/InputItem.jsx
@@ -1,20 +1,65 @@
 import React from "react";
 import 'typeface-roboto';
 import TextField from '@material-ui/core/TextField';
+import {
+    withStyles,
+    makeStyles,
+} from '@material-ui/core/styles';
 import styles from './InputItem.module.css';
-import  grey  from '@material-ui/core/colors/grey';
+import Button from '@material-ui/core/Button';
+
+const CssTextField = withStyles({
+    root: {
+        '& label.Mui-focused': {
+            color: 'pink',
+        },
+        '& .MuiInput-underline:after': {
+            borderBottomColor: 'pink',
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: 'grey',
+            },
+            '&:hover fieldset': {
+                borderColor: 'violet',
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: 'pink',
+            },
+        },
+    },
+})(TextField);
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    margin: {
+        margin: theme.spacing(1),
+    },
+}));
+
+
 
 
 const InputItem = () => {
+    const classes = useStyles();
     return (
-        <form>
-            <TextField
-                className={styles.input}
+        <form className={styles.input}>
+            <CssTextField
+                fullWidth
+                className={classes.margin}
                 label="Add new task here"
                 variant="outlined"
                 id="custom-css-outlined-input"
-                borderColor='grey'
             />
+                <Button
+                    color="secondary"
+                    href="#"
+                    className={styles.add}
+
+                >ADD</Button>
         </form>
     );
 };

0830825a4a9f249486b311d34d38401a5740ebe3
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-30 15:25:00 +0200

    data editing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,77 +9,121 @@ import styles from './App.module.css';
 
 
 class App extends React.Component {
+    idItem = 0;
+
     state = {
       items: [
-        {
-          label: 'Drink 2 glasses of water',
-          isDone: false,
-          isImportant: false,
-          textColor: 'black',
-          fontWeight: 400,
-          id: 1
-        },
-        {
-          label: 'Have a breakfast',
-          isDone: false,
-          isImportant: false,
-          textColor: 'black',
-          fontWeight: 400,
-          id: 2
-        },
-        {
-          label: 'Drink coffee',
-          isDone: false,
-          isImportant: false,
-          textColor: 'black',
-          fontWeight: 400,
-          id: 3
-        }
-      ],
-      count: 3
+        this.createTodoItem('Drink 2 glasses of water'),
+        this.createTodoItem('Have a breakfast'),
+        this.createTodoItem('Drink coffee')
+      ]
     };
+
+    createTodoItem(label) {
+      let newItem = {
+        label,
+        isDone: false,
+        isImportant: false,
+        textColor: 'black',
+        fontWeight: 400,
+        id: this.idItem += 1
+      };
+      return newItem;
+    }
 
     onClickDelete = (id) => {
       this.setState(({ items }) => {
         const itemIndex = items.findIndex((el) => el.id === id);
-        const newItems = [...items.slice(0, itemIndex), ...items.slice(itemIndex + 1)];
+        const newArray = [
+          ...items.slice(0, itemIndex),
+          ...items.slice(itemIndex + 1)
+        ];
 
-        return { items: newItems };
+        return { items: newArray };
       });
+
+      // another way:
+      // const newItemList = this.state.items.filter(
+      //   item => item.id !== id
+      // );
+      // this.setState({ items: newItemList });
     };
 
     onClickAdd = (text) => {
-      const newItem = {
-        label: text,
-        important: false,
-        isDone: false,
-        id: this.state.count + 1
-      };
+      const newItem = this.createTodoItem(text);
 
-      this.setState(({ items, count }) => {
+      this.setState(({ items }) => {
         const newArray = [
           ...items,
           newItem
         ];
 
-        return { items: newArray, count: count + 1 };
+        return { items: newArray };
       });
     };
 
+    toggleProperty(arr, id, stateItem, isImportant = false, fontWeight = 400) {
+      const itemIndex = arr.findIndex((el) => el.id === id);
+      const oldItem = arr[itemIndex];
+      const newItem = {
+        ...oldItem,
+        [stateItem]: !oldItem[stateItem],
+        isImportant,
+        fontWeight
+      };
+      return [
+        ...arr.slice(0, itemIndex),
+        newItem,
+        ...arr.slice(itemIndex + 1)
+      ];
+    }
+
     onClickDone = (id) => {
-      const newItemList = this.state.items.map(item => {
-        const newItem = { ...item };
-        if (item.id === id) {
-          newItem.isDone = !item.isDone;
-          newItem.isImportant = false;
-          newItem.fontWeight = 400;
-        }
-        return newItem;
+      this.setState(({ items }) => {
+        return {
+          items: this.toggleProperty(items, id, 'isDone')
+        };
       });
-      this.setState({ items: newItemList });
+      // this.setState(({ items }) =>{
+      //   const itemIndex = items.findIndex((el) => el.id === id);
+      //   const oldItem = items[itemIndex];
+      //   const newItem = {
+      //     ...oldItem,
+      //     isDone: !oldItem.isDone,
+      //     isImportant: false,
+      //     fontWeight: 400
+      //   };
+      //   const newArray = [
+      //     ...items.slice(0, itemIndex),
+      //     newItem,
+      //     ...items.slice(itemIndex + 1)
+      //   ];
+      //   return {
+      //     items: newArray
+      //   };
+      // });
+      // another way:
+      // const newItemList = this.state.items.map(item => {
+      //   const newItem = { ...item };
+      //   if (item.id === id) {
+      //     newItem.isDone = !item.isDone;
+      //     newItem.isImportant = false;
+      //     newItem.fontWeight = 400;
+      //   }
+      //   return newItem;
+      // });
+      // this.setState({ items: newItemList });
     };
 
     onClickImportant = (id) => {
+      // this.setState(({ items }) => {
+      //   const itemIndex = items.findIndex((el) => el.id === id);
+      //   if (!items[itemIndex].isDone) {
+      //     return {
+      //       items: this.toggleProperty( items, id, 'isImportant')
+      //     };
+      //   }
+      // });
       const newItemList = this.state.items.map(item => {
         const newItem = { ...item };
         const newFontWeight = item.fontWeight === 400 ? 600 : 400;
@@ -97,17 +141,20 @@ class App extends React.Component {
     };
 
     render() {
+      const { items } = this.state;
+      const doneCount = items.filter((el) => el.isDone).length;
+      const todoCount = items.length - doneCount;
       return (
             <Container maxWidth="sm" className={styles.container}>
                 < SearchItem/>
                 < ItemList
-                    items={this.state.items}
+                    items={items}
                     onClickDelete={this.onClickDelete}
                     onClickDone={this.onClickDone}
                     onClickImportant={this.onClickImportant}
                 />
                 < InputItem onClickAdd={this.onClickAdd}/>
-                < Footer/>
+                < Footer toDo={todoCount} done={doneCount}/>
             </Container>
       );
     }
